410d7ed5d3caa13a4b034b14af525e0a12e14d1a
Saravanaraja <saravanarocks@gmail.com>
2021-08-16 18:43:24 +0530

    ThinkBridge WebSite Testing
    Testing Website testing

---
+++ TravelWebsitePreparation/WebLoginTest/WebLoginTest/LoginClass.cs
@@ -0,0 +1,96 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLoginTest
+{
+    public class LoginClass
+    {
+        public ChromeWebdriver chromewebDriver = new ChromeWebdriver();
+        public IWebDriver webDriver;
+        public List<string> languagesread = new List<string>();
+
+        public string langDropdown = @"/html/body/div[1]/div[2]/section/div/div[2]/section/div/div[1]/span";
+        public string English = @"ui-select-choices-row-1-0";
+        public string Dutch = @"//div[@id='ui-select-choices-row-1-1']";
+        public string userNametextboxID = "name";
+        public string organizationTextBoxID = "orgName";
+        public string emailTextBoxID = "singUpEmail";
+        public string agreeCheckBox = @"/html/body/div[1]/div[2]/section/div/div[3]/div/section/div[1]/form/fieldset/div[4]/label/span";
+        public string signUpButtonXpath = @"/html/body/div[1]/div[2]/section/div/div[3]/div/section/div[1]/form/fieldset/div[5]";
+        public string signupMessageXpath = @"/html/body/div[1]/div[2]/section/div/div[3]/div/section/div[1]/form/div/span";
+
+        public IWebElement langdropdownbutton => webDriver.FindElement(By.XPath(langDropdown));
+        public IWebElement LangEnglish => webDriver.FindElement(By.Id(English));
+        public IWebElement langDutch => webDriver.FindElement(By.XPath(Dutch));
+
+        public IWebElement userNameTextBox => webDriver.FindElement(By.Id(userNametextboxID));
+        public IWebElement emailTextBox => webDriver.FindElement(By.Id(emailTextBoxID));
+        public IWebElement organizationTextBox => webDriver.FindElement(By.Id(organizationTextBoxID));
+        public IWebElement agreeCheck => webDriver.FindElement(By.XPath(agreeCheckBox));
+
+        public IWebElement signUpButton => webDriver.FindElement(By.XPath(signUpButtonXpath));
+
+        public IWebElement signUpMessage => webDriver.FindElement(By.XPath(signupMessageXpath));
+
+
+
+       // Method to Launch the browser and intitalize the wedDriver element
+        public void launchBrowser()
+
+        {
+            webDriver = chromewebDriver.webdriverInitialization();
+        }
+
+        //Method to Navigate to URL specified
+        public void NaviatetoUrl(string url)
+        {
+            webDriver.Navigate().GoToUrl(url);
+        }
+
+        // Method to select language in the web page
+        public void LanguageSelection()
+        {
+            ////select = new SelectElement(langdropdownbutton);
+            langdropdownbutton.Click();
+
+        }
+
+        // Method to return list of languages available in the drop down
+        public List<string> GetLanguages()
+        {
+
+            languagesread.Add(langDutch.GetAttribute("innerText"));
+            languagesread.Add(LangEnglish.GetAttribute("innerText"));
+            return languagesread;
+        }
+
+        // Method to sign up to the web page
+        public void SignUp(string UserName, string organizationName, string email)
+        {
+            userNameTextBox.Click();
+            userNameTextBox.SendKeys(UserName);
+            organizationTextBox.Click();
+            organizationTextBox.SendKeys(organizationName);
+            emailTextBox.Click();
+            emailTextBox.SendKeys(email);
+            agreeCheck.Click();
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(20));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(signUpButtonXpath)));
+            signUpButton.Click();
+        }
+
+        // Method to get the Signup message
+        public string SignUpMessage()
+        {
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(20));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(signupMessageXpath)));
+            var test = signUpMessage.GetAttribute("innerText");
+            return test;
+        }
+    }
+}
---
+++ TravelWebsitePreparation/WebLoginTest/WebLoginTest/Utilities/Mailcheck.cs
@@ -0,0 +1,38 @@
+﻿using AE.Net.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLoginTest.Utilities
+{
+    public class Mailcheck
+    {
+        public string YourUserName = "Saravanarocks@gmail.com";
+        public string Password = "kyllimrrbmpogtxb";
+        public ImapClient IC;
+        // Method to connect to the gmail account to verify whether mail is received. Subject is verified 
+        public bool Mailavailable()
+        {
+            try
+            {
+                IC = new ImapClient("imap.gmail.com", YourUserName, Password, AuthMethods.Login, 993, false);
+                IC.SelectMailbox("INBOX");
+                var Email = IC.GetMessage(0);
+                var searchresult = IC.Search(SearchCondition.Subject("Hello Saravana"));
+                if (searchresult.Count() > 0)
+                {
+                    return true;
+                }
+            }
+
+            catch (Exception e)
+            {
+
+            }
+            return false;
+        }
+
+    }
+}
